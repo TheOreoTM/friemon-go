9d3a951454074d2b0a3913f28783564b677ed53a
TheOreoTM <qwertzybad@gmail.com>
2025-05-26 19:35:38 +0500

    feat: new migrations

--- friemon/db/migrations/000002_create_users_table.up.sql
+++ friemon/db/migrations/000002_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id VARCHAR(255) PRIMARY KEY,
     balance INT NOT NULL DEFAULT 0,
-    selected_id UUID,
+    selected_id UUID DEFAULT NULL,
     order_by INT NOT NULL DEFAULT 0,
     order_desc BOOLEAN NOT NULL DEFAULT FALSE,
     shinies_caught INT NOT NULL DEFAULT 0,
