df0f32c99ab578cbb705b987d757e78850ceaf1a
TheOreoTM <qwertzybad@gmail.com>
2025-06-13 12:05:55 +0500

    fix: new order

--- cmd/friemon/main.go
+++ cmd/friemon/main.go
@@ -80,11 +80,6 @@ func main() {
 	b := bot.New(*cfg, buildInfo, ctx)
 	r := handler.New()
 
-	// Setup bot with event listeners
-	if err := b.SetupBot(handlers.OnMessage(b)); err != nil {
-		log.Fatal("Failed to setup bot", logger.ErrorField(err))
-	}
-
 	r.Use(middleware.Logger)
 	r.Group(func(router handler.Router) {
 		r.Use(middleware.Print("companion"))
@@ -101,6 +96,11 @@ func main() {
 	})
 
 	b.Client.AddEventListeners(r)
+
+	// Setup bot with event listeners
+	if err := b.SetupBot(handlers.OnMessage(b)); err != nil {
+		log.Fatal("Failed to setup bot", logger.ErrorField(err))
+	}
 
 	// Connect to Discord
 	if err := b.Client.OpenGateway(ctx); err != nil {
